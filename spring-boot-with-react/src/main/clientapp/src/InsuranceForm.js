afbd00e0dd3be9546ec598c7f061c9b818b2f798
ikg <>
2022-09-25 20:31:35 +0200

    Started validationHandling (not finished)

--- spring-boot-with-react/src/main/clientapp/src/InsuranceForm.js
+++ spring-boot-with-react/src/main/clientapp/src/InsuranceForm.js
@@ -3,68 +3,94 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 import './index.css'
 
-// Use direct import to reduce the amount of code sendt to the client
 
-const InsuranceForm = () => (
-        <Form>
-            <Form.Group className='mb-3' controlId='formRegistrationNumber'>
-                <Form.Label>Bilens registreringsnummer</Form.Label>
-                <Form.Control 
-                    type='number'
-                    placeholder='Feks: AB 12345'/>
-            </Form.Group>
 
-            <Form.Group className='mb-3' controlId='formInsuranceBonus'>
-                <Form.Label>Din bonus</Form.Label>
-                {/*TODO: Add correct type*/}
+
+const InsuranceForm = () => {
+    
+    const [validated, setValidated] = useState(false);
+        
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+
+        if(form.checkValidity() === false){
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+    return(
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group className='mb-3' controlId='formRegistrationNumber'>
+            <Form.Label>Bilens registreringsnummer</Form.Label>
+            <Form.Control
+                required
+                type='number'
+                placeholder='Feks: AB12345'
+                pattern=''/>
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='formInsuranceBonus'>
+            <Form.Label>Din bonus</Form.Label>
+            {/*TODO: Add correct type*/}
+            <Form.Control
+                required
+                type='number'
+                placeholder='Placeholder'/>
+            <Form.Text>
+                Har du ikke oppspart bonus?
+                Alle våre kunder starter med x% i oppstartsbonus!
+            </Form.Text>
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='formNationalIdentityNumber'>
+            <Form.Label>Fødselsnummer</Form.Label>
+            <Form.Control
+                required
+                type='number'
+                placeholder='11 siffer'
+                pattern='[0-9]{11}'/>
+        </Form.Group>
+        <Row className='mb-3'>
+            <Form.Group as={Col} controlId='formSurName'>
+                <Form.Label>Fornavn</Form.Label>
                 <Form.Control
-                    type='number'
-                    placeholder='Placeholder'/>
-                <Form.Text>
-                    Har du ikke oppspart bonus?
-                    Alle våre kunder starter med x% i oppstartsbonus!
-                </Form.Text>
+                    required
+                    type='text'/>
             </Form.Group>
 
-            <Form.Group className='mb-3' controlId='formNationalIdentityNumber'>
-                <Form.Label>Fødselsnummer</Form.Label>
+            <Form.Group as={Col} controlId='formLastName'>
+                <Form.Label>Etternavn</Form.Label>
                 <Form.Control
-                    type='number'
-                    placeholder='11 siffer'/>
+                    required
+                    type='text'/>
             </Form.Group>
-            <Row className='mb-3'>
-                <Form.Group as={Col} controlId='formSurName'>
-                    <Form.Label>Fornavn</Form.Label>
-                    <Form.Control
-                        type='text'/>
-                </Form.Group>
+        </Row>
 
-                <Form.Group as={Col} controlId='formLastName'>
-                    <Form.Label>Etternavn</Form.Label>
-                    <Form.Control
-                        type='text'/>
-                </Form.Group>
-            </Row>
+        <Form.Group className='mb-3' controlId='formEmail'>
+            <Form.Label>E-post</Form.Label>
+            <Form.Control
+                required
+                type='email'
+                placeholder='test@gmail.com'/>
+            <Form.Text>
+                
+            </Form.Text>
+        </Form.Group>
 
-            <Form.Group className='mb-3' controlId='formEmail'>
-                <Form.Label>E-post</Form.Label>
-                <Form.Control
-                    type='email'
-                    placeholder='test@gmail.com'/>
-                <Form.Text>
-                    
-                </Form.Text>
-            </Form.Group>
-
-            <Button variant='dark' type=''>
-                Kjøp
-            </Button>
-            <Button variant='outline-dark' type='reset'>
-                Avbryt
-            </Button>
-        </Form>
-)
+        <Button variant='dark' type=''>
+            Kjøp
+        </Button>
+        <Button variant='outline-dark' type='reset'>
+            Avbryt
+        </Button>
+    </Form>
+    )
+}
 
 export default InsuranceForm
